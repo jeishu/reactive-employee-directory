7069140ca8ccad045e7f4b91bead79643d8b82a3
jeishu <jeremy.zhu@utexas.edu>
2021-03-04 20:56:22 -0600

    Started some initial files for styling and components

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Jumbotron/Jumbotron.scss";
+
+const Jumbotron = () => {
+    return (
+        <div className="Jumbotron">
+            
+        </div>
+    )
+}
+
+export default Jumbotron
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Navbar/Navbar.scss";
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar;
