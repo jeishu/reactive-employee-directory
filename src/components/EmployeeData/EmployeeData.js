8ef1406877da09873469ac58bdca75a55475c10e
jeishu <jeremy.zhu@utexas.edu>
2021-03-10 18:26:48 -0600

    Added a map.array to render each card for every random generated employee.

--- src/components/EmployeeData/EmployeeData.js
+++ src/components/EmployeeData/EmployeeData.js
@@ -4,28 +4,43 @@ import API from "../../utils/API.js";
 import Card from "../Card/Card.js";
 
 const EmployeeData = () => {
-    const [image, setImage] = useState();
-    const [name, setName] = useState();
-    const [phone, setPhone] = useState();
-    const [email, setEmail] = useState();
+    // const [image, setImage] = useState();
+    // const [name, setName] = useState();
+    // const [phone, setPhone] = useState();
+    // const [email, setEmail] = useState();
 
+    const [results, setResults] = useState([]);
+
+    // Render 25 People
     useEffect(() => {
         API.getEmployee().then(res => {
-            setImage(res.data.results[0].picture.large);
-            setName(`${res.data.results[0].name.first} ${res.data.results[0].name.last}`);
-            setPhone(res.data.results[0].phone);
-            setEmail(res.data.results[0].email);
+            // setImage(res.data.results[0].picture.large);
+            // setName(`${res.data.results[0].name.first} ${res.data.results[0].name.last}`);
+            // setPhone(res.data.results[0].phone);
+            // setEmail(res.data.results[0].email);
+
+            // Storing an array of 100 people
+            setResults(res.data.results);
+            console.log(results);
         })
     }, []);
 
     return (
         <div>
-            <Card
+            {/* <Card
                 image={image}
                 name={name}
                 phone={phone}
                 email={email}
-                />
+                /> */}
+            {results.map ( employee => {
+                return (<Card
+                image={employee.picture.large}
+                name={`${employee.name.first} ${employee.name.last}`}
+                phone={employee.phone}
+                email={employee.email}
+                />)
+            })}    
         </div>
     )
 }
